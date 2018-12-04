cc09ecc79e363ea977413858a3b4939ab7cac25d
Hamid Rahimi <mail@airahimi.no>
2018-12-04 15:14:13 +0100

    Added button images

--- src/main/java/no/airahimi/hashing_directories/MainGUI.java
+++ src/main/java/no/airahimi/hashing_directories/MainGUI.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -29,51 +30,59 @@ public class MainGUI extends Application {
 
         final FileChooser fileChooser = new FileChooser();
         final DirectoryChooser directoryChooser = new DirectoryChooser();
-        final TextField fileTextField = new TextField("Click to select file to checksum.");
+        final TextField fileTextField = new TextField("No files or folder selected at the moment.");
         final TextField hashTextField = new TextField("Click to select checksum file.");
 
         fileTextField.setEditable(false);
         hashTextField.setEditable(false);
 
-
         final ToggleGroup fileGroup = new ToggleGroup();
-        final ToggleButton fileButton = new ToggleButton("FILE");
-        fileButton.setGraphic(new ImageView());
-        final ToggleButton folderButton = new ToggleButton("FOLDER");
-        final ToggleGroup functionGroup = new ToggleGroup();
-        final ToggleButton hashButton = new ToggleButton("HASH");
-        final ToggleButton checkButton = new ToggleButton("CHECK");
+        final ToggleButton fileButton = new ToggleButton();
+        final ImageView fileView = new ImageView(new Image("file:src/main/resources/fileicon.png"));
+        fileView.fitWidthProperty().bindBidirectional(fileButton.minWidthProperty());
+        fileView.setPreserveRatio(true);
+        fileButton.setGraphic(fileView);
 
+        final ToggleButton folderButton = new ToggleButton();
+        final ImageView folderView = new ImageView(new Image("file:src/main/resources/foldericon.png"));
+        folderView.fitWidthProperty().bindBidirectional(folderButton.minWidthProperty());
+        folderView.setPreserveRatio(true);
+        folderButton.setGraphic(folderView);
+
+        final ToggleGroup functionGroup = new ToggleGroup();
+
+        final ToggleButton hashButton = new ToggleButton("HASH");
         fileButton.setMinWidth(59);
         folderButton.setMinWidth(59);
         fileButton.setToggleGroup(fileGroup);
         folderButton.setToggleGroup(fileGroup);
         fileButton.setSelected(true);
+        final ToggleButton checkButton = new ToggleButton("CHECK");
         hashButton.setMinWidth(59);
         checkButton.setMinWidth(59);
         hashButton.setToggleGroup(functionGroup);
         checkButton.setToggleGroup(functionGroup);
         hashButton.setSelected(true);
 
-        fileTextField.setOnMouseClicked(
-            e-> {
-                if (fileButton.isSelected()) {
+        fileButton.setOnAction(
+                e-> {
                     fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-                    fileChooser.setTitle("Select file to check or hash");
+                    fileChooser.setTitle("Select file");
                     File file = fileChooser.showOpenDialog(primaryStage);
-                    if (file != null) {
-                        fileTextField.setText(file.getAbsolutePath());
-                    }
-                } else if (folderButton.isSelected()) {
-                    directoryChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-                    directoryChooser.setTitle("Select folder to check or hash");
-                    File file = directoryChooser.showDialog(primaryStage);
-                    if (file != null && file.isDirectory()) {
-                        fileTextField.setText(file.getAbsolutePath());
-                    }
-                }
 
-            }
+                    if (file != null)
+                        fileTextField.setText(file.getAbsolutePath());
+                }
+        );
+        folderButton.setOnAction(
+                e-> {
+                    directoryChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+                    directoryChooser.setTitle("Select folder");
+                    File file = directoryChooser.showDialog(primaryStage);
+
+                    if (file != null && file.isDirectory())
+                        fileTextField.setText(file.getAbsolutePath());
+                }
         );
 
         hashButton.setOnAction(
@@ -125,10 +134,15 @@ public class MainGUI extends Application {
         primaryStage.widthProperty().addListener(
                 (obs, oldVal, newVal) -> {
                     inputGridPane.prefWidthProperty().setValue(primaryStage.getWidth());
-                    fileButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
 
+                    fileButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
                     folderButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
                     fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3).multiply(2));
+
+                    hashButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+                    checkButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+                    fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3).multiply(2));
+
                 }
         );
 
@@ -140,6 +154,8 @@ public class MainGUI extends Application {
 
 
         primaryStage.setScene(scene);
+        primaryStage.setMinWidth(460);
+        primaryStage.setMinHeight(350);
         primaryStage.show();
     }
 
