ffa7718cc7379e66c519b0e48dab80c7cc8d3517
airahimi <mail@airahimi.no>
2018-12-04 23:02:37 +0100

    Refactoring

--- src/main/java/no/airahimi/hashing_directories/gui/Main.java
+++ src/main/java/no/airahimi/hashing_directories/gui/Main.java
@@ -23,8 +23,47 @@ public class Main extends Application {
     public void start(final Stage primaryStage) {
         primaryStage.setTitle("Checksum");
 
+        final Pane rootGroup = new VBox(12);
+        initUI(rootGroup);
+        rootGroup.setPadding(new Insets(12, 12, 12 , 12));
+
+        Scene scene = new Scene(rootGroup, 720, 445);
+
+
+        scene.widthProperty().addListener(
+                (obs, oldVal, newVal) -> {
+                }
+        );
+
+        scene.heightProperty().addListener(
+                (obs, oldVal, newVal) -> {
+
+        }
+        );
+
+        File file = new File("src/main/resources/Main.css");
+        scene.getStylesheets().add("file:///" + file.getAbsolutePath().replace("\\", "/"));
+
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(460);
+        primaryStage.setMinHeight(350);
+        primaryStage.minWidthProperty().bind(scene.heightProperty().multiply(1.5));
+        primaryStage.minHeightProperty().bind(scene.widthProperty().divide(1.5));
+        primaryStage.show();
+    }
+
+    private void initUI(Pane root) {
+        final GridPane gridPane = initGridPane();
+        final TextArea textArea = initTextArea();
+        final HBox hBox  = initHBox();
+
+        root.getChildren().addAll(gridPane, textArea, hBox);
+    }
+
+    private GridPane initGridPane() {
         final FileChooser fileChooser = new FileChooser();
         final DirectoryChooser directoryChooser = new DirectoryChooser();
+
         final TextField fileTextField = new TextField("No file/folder selected.");
         final TextField hashTextField = new TextField("No file selected at the moment.");
 
@@ -32,31 +71,33 @@ public class Main extends Application {
         hashTextField.setEditable(false);
 
         final ToggleGroup fileGroup = new ToggleGroup();
-        final ToggleButton fileButton = new ToggleButton();
-        final ImageView fileView = new ImageView(new Image("file:src/main/resources/fileicon.png"));
-        fileView.setPreserveRatio(true);
-        fileButton.setGraphic(fileView);
-        fileButton.setMinWidth(60);
-        fileButton.setToggleGroup(fileGroup);
-
-        final ToggleButton folderButton = new ToggleButton();
-        final ImageView folderView = new ImageView(new Image("file:src/main/resources/foldericon.png"));
-        folderView.setPreserveRatio(true);
-        folderButton.setGraphic(folderView);
-        folderButton.setMinWidth(60);
-        folderButton.setToggleGroup(fileGroup);
-
         final ToggleGroup functionGroup = new ToggleGroup();
+
+        final ToggleButton fileButton = new ToggleButton();
+        final ToggleButton folderButton = new ToggleButton();
         final ToggleButton hashButton = new ToggleButton("HASH");
         final ToggleButton checkButton = new ToggleButton("CHECK");
+
         hashButton.setMinWidth(60);
         checkButton.setMinWidth(60);
         hashButton.setToggleGroup(functionGroup);
         checkButton.setToggleGroup(functionGroup);
 
-        final TextArea textArea = new TextArea(" hey");
+        final ImageView fileView = new ImageView(new Image("file:src/main/resources/fileicon.png"));
+        final ImageView folderView = new ImageView(new Image("file:src/main/resources/foldericon.png"));
+
+        fileView.setPreserveRatio(true);
+        fileButton.setGraphic(fileView);
+        fileButton.setMinWidth(60);
+        fileButton.setToggleGroup(fileGroup);
+        folderView.setPreserveRatio(true);
+        folderButton.setGraphic(folderView);
+        folderButton.setMinWidth(60);
+        folderButton.setToggleGroup(fileGroup);
 
 
+
+        /**
         fileButton.setOnAction(
                 e-> {
                     fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
@@ -91,27 +132,27 @@ public class Main extends Application {
         );
 
         hashButton.setOnAction(
-            e-> {
-                fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-                fileChooser.setTitle("Select file to write hashes to");
-                File file = fileChooser.showSaveDialog(primaryStage);
+                e-> {
+                    fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+                    fileChooser.setTitle("Select file to write hashes to");
+                    File file = fileChooser.showSaveDialog(primaryStage);
 
-                if (file != null)
-                    hashTextField.setText(file.getAbsolutePath());
-            }
+                    if (file != null)
+                        hashTextField.setText(file.getAbsolutePath());
+                }
         );
 
         hashTextField.setOnMouseClicked(
-            e-> {
-                fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-                fileChooser.setTitle("Select hash file");
-                File file = fileChooser.showOpenDialog(primaryStage);
+                e-> {
+                    fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+                    fileChooser.setTitle("Select hash file");
+                    File file = fileChooser.showOpenDialog(primaryStage);
 
-                if (file != null)
-                    hashTextField.setText(file.getAbsolutePath());
-            }
+                    if (file != null)
+                        hashTextField.setText(file.getAbsolutePath());
+                }
         );
-
+        */
 
         final GridPane inputGridPane = new GridPane();
         GridPane.setConstraints(fileButton, 0, 0);
@@ -126,11 +167,50 @@ public class Main extends Application {
         inputGridPane.setHgap(10);
         inputGridPane.setVgap(10);
 
-        final Pane rootGroup = new VBox(12);
-        Scene scene = new Scene(rootGroup, 720, 445);
-        rootGroup.getChildren().addAll(inputGridPane, textArea);
-        rootGroup.setPadding(new Insets(12, 12, 12 , 12));
 
+        //Width-Responsivity
+        //inputGridPane.prefWidthProperty().setValue(primaryStage.getWidth());
+
+        fileView.fitWidthProperty().bindBidirectional(fileButton.minWidthProperty());
+        folderView.fitWidthProperty().bindBidirectional(folderButton.minWidthProperty());
+
+        fileButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+        folderButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+        fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6).multiply(4));
+
+        hashButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+        checkButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
+        fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6).multiply(4));
+
+        /*
+
+        resetView.fitWidthProperty().bindBidirectional(resetButton.minWidthProperty());
+        playView.fitWidthProperty().bindBidirectional(playButton.minWidthProperty());
+        saveView.fitWidthProperty().bindBidirectional(saveButton.minWidthProperty());
+
+        resetButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));
+        playButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));
+        saveButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));*/
+
+        return inputGridPane;
+    }
+
+    private TextArea initTextArea() {
+
+        TextArea textArea = new TextArea(" hey");
+        textArea.setEditable(false);
+
+        /**
+         // Height-Responsivity
+         textArea.prefHeightProperty().bind(
+         scene.heightProperty().subtract(inputGridPane.getHeight()));*/
+
+        return textArea;
+
+    }
+
+    private HBox initHBox() {
+        HBox hBox = new HBox();
 
         final ImageView resetView = new ImageView(new Image("file:src/main/resources/reseticon.png"));
         final ImageView playView = new ImageView(new Image("file:src/main/resources/playicon.png"));
@@ -150,58 +230,10 @@ public class Main extends Application {
         resetButton.setMinWidth(60);
         playButton.setMinWidth(60);
         saveButton.setMinWidth(60);
-        final HBox hBox = new HBox();
         hBox.setSpacing(12);
         hBox.getChildren().addAll(resetButton, playButton, saveButton);
-        rootGroup.getChildren().addAll(hBox);
 
-
-        //Width-Responsivity
-        //inputGridPane.prefWidthProperty().setValue(primaryStage.getWidth());
-
-        fileView.fitWidthProperty().bindBidirectional(fileButton.minWidthProperty());
-        folderView.fitWidthProperty().bindBidirectional(folderButton.minWidthProperty());
-
-        fileButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
-        folderButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
-        fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6).multiply(4));
-
-        hashButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
-        checkButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6));
-        fileTextField.prefWidthProperty().bind(inputGridPane.widthProperty().divide(6).multiply(4));
-
-
-        resetView.fitWidthProperty().bindBidirectional(resetButton.minWidthProperty());
-        playView.fitWidthProperty().bindBidirectional(playButton.minWidthProperty());
-        saveView.fitWidthProperty().bindBidirectional(saveButton.minWidthProperty());
-
-        resetButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));
-        playButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));
-        saveButton.prefWidthProperty().bind(inputGridPane.widthProperty().divide(3));
-
-        // Height-Responsivity
-        textArea.prefHeightProperty().bind(
-                scene.heightProperty().subtract(inputGridPane.getHeight()));
-
-
-        scene.widthProperty().addListener(
-                (obs, oldVal, newVal) -> {
-                }
-        );
-
-        scene.heightProperty().addListener(
-                (obs, oldVal, newVal) -> {
-
-        }
-        );
-
-        File file = new File("src/main/resources/Main.css");
-        scene.getStylesheets().add("file:///" + file.getAbsolutePath().replace("\\", "/"));
-
-        primaryStage.setScene(scene);
-        primaryStage.setMinWidth(460);
-        primaryStage.setMinHeight(350);
-        primaryStage.show();
+        return hBox;
     }
 
 }
